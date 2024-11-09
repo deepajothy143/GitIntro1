1ff317034f7cfe3e06c7396ca8a8354daf93eee5
deepajothy <deepabalu892@gmail.com>
2024-11-09 07:28:07 +0530

    done

--- src/org/git/Gitpratice.java
+++ src/org/git/Gitpratice.java
@@ -1,5 +1,23 @@
 package org.git;
 
-public class Gitpratice {
 
+public class Gitpratice {
+	private void empId() {
+		System.out.println("52789");
+
+		}
+		private void empName() {
+		System.out.println("deepika");
+
+		}
+		private void empDob() {
+		System.out.println("17-12-1998");
+
+		}
+		public static void main(String[] args) {
+			Gitpratice g=new Gitpratice();
+			g.empId();
+			g.empName();
+			g.empDob();
+}
 }
